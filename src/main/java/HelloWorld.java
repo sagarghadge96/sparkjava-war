c64434329f9681dc6166b45b686204e3b646b12f
sagarghadge96 <sagarghadge96@gmail.com>
2026-04-09 23:30:51 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello World and hello sagar welcome toooo devops");
+		get("/hello", (req, res) -> "Hello World and hello sagar welcome to devops");
 	}
 }
